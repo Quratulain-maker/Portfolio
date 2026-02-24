70ca3d9ec5fe942dc4e2ef0b98473674365d9473
Quratulain-maker <quratulainabidhussain15@gmail.com>
2026-02-25 00:36:56 +0500

    Add files via upload

---
+++ internship projects/Live Camera Translation App/live_translate_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LiveTranslatePage extends StatefulWidget{
+  @override
+  State<LiveTranslatePage> createState() => _LiveTranslatePageState();
+}
+
+class _LiveTranslatePageState extends State<LiveTranslatePage> {
+  @override
+  Widget build(BuildContext context) {
+ return Scaffold(
+
+ );
+  }
+}
